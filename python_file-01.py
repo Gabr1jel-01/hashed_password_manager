455ea387c299c660ce15d2d96011f2cdb11e6b0d
Gabrijel <g.gordic@outlook.com>
2023-02-25 12:07:53 +0100

    Update python_file-01.py

--- python_file-01.py
+++ python_file-01.py
@@ -1,5 +1,21 @@
 import random
+from tkinter import *
 from cryptography.fernet import Fernet
+from functools import partial
+
+
+def validateLogin(user_name, user_password):
+    print("Username entered: ", user_name.get())
+    print("Password entered: ", user_password.get())
+
+appWindow = Tk()
+appWindow.geometry("300150")
+appWindow.title("Login")
+
+
+user_nameLabel = Label
+
+
 
 passlen = int(input("Enter how many characters: "))
 s='''abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?
@@ -17,6 +33,7 @@ encPassword = fernet.encrypt(password.encode())
 
 
 print(password)
+
 print(encPassword)
 
 
