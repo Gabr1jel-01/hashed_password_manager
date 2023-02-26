bed972ff0e035e590dfaf91175797f62b9b18786
Gabrijel <g.gordic@outlook.com>
2023-02-26 01:47:58 +0100

    Update python_file-01.py

--- python_file-01.py
+++ python_file-01.py
@@ -13,16 +13,16 @@ appWindow.geometry("300x150")
 appWindow.title("Login")
 
 # username label and text entry box
-user_nameLabel = Label(appWindow, text="Username").grid(row=0, column=0)#er_name = StringVar()
+user_nameLabel = Label(appWindow, text="Username").grid(row=0, column=0)
 user_name = StringVar()
 user_nameEntry = Entry(appWindow,textvariable=user_name).grid(row=0,column=1)
 
-passwordLabel = Label(appWindow, text ="Password").grid(row=0, column=2)
-password = StringVar()
-password_entry_box = Entry(appWindow,textvariable=password).grid(row=0, column = 2)
+
+#passwordLabel = Label(appWindow, text ="Password").grid(row=0, column=2)
+#password = StringVar()
+#password_entry_box = Entry(appWindow,textvariable=password).grid(row=0, column = 2)
 
 
-"""
 passlen = int(input("Enter how many characters: "))
 s='''abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?
      abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
@@ -47,4 +47,3 @@ decPassword = fernet.decrypt(encPassword).decode()
 
 print(decPassword)
 
-"""
