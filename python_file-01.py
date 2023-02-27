824afa64c11af37a9d4400917b54ed011fa55e22
Gabrijel <g.gordic@outlook.com>
2023-02-27 22:50:51 +0100

    Update python_file-01.py

--- python_file-01.py
+++ python_file-01.py
@@ -11,18 +11,26 @@ def validateLogin(user_name, user_password):
 appWindow = Tk()
 appWindow.geometry("300x150")
 appWindow.title("Login")
+appWindow.configure(bg="#c7d1af")
 
 # username label and text entry box
 user_nameLabel = Label(appWindow, text="Username").grid(row=0, column=0)
 user_name = StringVar()
-user_name_entry_box = Entry(appWindow,textvariable=user_name).grid(row=0,column=1)
+user_name_entry_box = Entry(appWindow,textvariable=user_name, bg="#bcbcbc").grid(row=0,column=1)
 
-
+#password label amd text entry box
 passwordLabel = Label(appWindow, text ="Password").grid(row=1, column=0)
 password = StringVar()
-password_entry_box = Entry(appWindow,textvariable=password).grid(row=1, column = 1)
+password_entry_box = Entry(appWindow,textvariable=password, bg="#bcbcbc").grid(row=1, column = 1)
+
+#submit button
+
+submit_button = Button(appWindow, text= "Submit", bd=3, activebackground="#3e6dbf").grid(row=2, column=0)
 
 
+
+
+"""
 passlen = int(input("Enter how many characters: "))
 s='''abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?
      abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
@@ -47,4 +55,6 @@ decPassword = fernet.decrypt(encPassword).decode()
 
 print(decPassword)
 
-#
+
+"""
+appWindow.mainloop()
