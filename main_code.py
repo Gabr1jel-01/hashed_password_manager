0d9c17aa6fb7092a7d9b6515caa2067db72a8a68
Gabrijel <g.gordic@outlook.com>
2023-03-13 21:26:02 +0100

    Nove izmjene
    Povecao sam box gdje se unose podaci i sifra, te sam prilikom unosa sifre sakrio istu, pokazuje * umjesto unesenog znaka

--- main_code.py
+++ main_code.py
@@ -16,17 +16,17 @@ appWindow.configure(bg="#c7d1af")
 # username label and text entry box
 user_nameLabel = Label(appWindow, text="Username",bg="#bcbcbc").grid(row=0, column=0)
 user_name = StringVar()
-user_name_entry_box = Entry(appWindow,textvariable=user_name, bg="#bcbcbc").grid(row=0,column=1)
+user_name_entry_box = Entry(appWindow,width='30',textvariable=user_name, bg="#bcbcbc").grid(row=0,column=1)
 
 #password label and text entry box
 passwordLabel = Label(appWindow, text ="Password",bg="#bcbcbc").grid(row=1, column=0)
 password = StringVar()
-password_entry_box = Entry(appWindow,textvariable=password, bg="#bcbcbc").grid(row=1, column = 1)
+password_entry_box = Entry(appWindow,width='30',textvariable=password, bg="#bcbcbc", show='*').grid(row=1, column = 1)
 
 #double_check_password label and text entry box
 double_check_passwordLabel = Label(appWindow, text="Re-enter",bg="#bcbcbc").grid(row=2,column=0)
 double_check_password = StringVar()
-double_check_password_entry_box = Entry(appWindow, textvariable=double_check_password, bg="#bcbcbc").grid(row=2, column=1)
+double_check_password_entry_box = Entry(appWindow,width='30', textvariable=double_check_password, bg="#bcbcbc",show='*').grid(row=2, column=1)
 
 def submit_button_logic():
     
